5ad05a774124110c2d3e786d84b444f6926ca803
devBublik <ew.sorokina2011@yandex.ru>
2020-10-26 22:13:49 +0300

    add first page

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,16 @@
 $(function () {
 
-	$('.slider__products').slick({
-		prevArrow:'<button class="slick-arrow slick-prev"> <img src="images/icons/previous.png" alt=""></button>',
-		nextArrow:'<button class="slick-arrow slick-next"> <img src="images/icons/next.png" alt=""></button>',
-	})
 
-	$('.rate-category').rating({
+	$('.slider__product').slick({
 
-	})
+	});
 
+	$('.slider__product-followers').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1
+
+
+	});
 
 
 
