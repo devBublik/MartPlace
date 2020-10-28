69d44c48984977ee93dfa25340aaec0eae06c4f2
devBublik <ew.sorokina2011@yandex.ru>
2020-10-28 22:32:38 +0300

    finished page category

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,30 @@
 $(function () {
 
 
-	$('.slider__product').slick({
-
-	});
+	$('.slider__product').slick({});
 
 	$('.slider__product-followers').slick({
 		slidesToShow: 3,
 		slidesToScroll: 1
-
-
 	});
 
+	$(".js-range-slider").ionRangeSlider({
+		type: "double",
+		from: 30,
+		to: 300,
+		prefix: "$"
+	});
 
+	$(".sorted__rate").rateYo({
+		ratedFill: "#FFC000",
+		spacing: '3px',
+		numStars: 5,
+		starWidth: "15px",
+		rating: 5,
+		readOnly: true
+	});
 
-
+	var mixer = mixitup('.product__filtered');
 
 
 
